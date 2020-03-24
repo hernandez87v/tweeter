08301567d359b892f1aeba7a35872d7ed694c059
hernandez87v <hernandez87v@hotmail.com>
2020-03-24 15:22:50 -0700

    Added counter function and fixed up layout

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  $('textarea').on('input', function() {
+    let count = $(this).val();
+    let limit = 140 - count.length;
+
+    if (limit <= 0) {
+      $('.counter').css({ color: 'red' });
+    } else if (limit <= 20) {
+      $('.counter').css({ color: 'peru' });
+    } else if (limit >= 21) {
+      $('.counter').css({ color: 'black' });
+    }
+    $('.counter').text(limit);
+  });
+});
