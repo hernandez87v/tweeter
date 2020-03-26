12d1b9c3f2e803d89bc987152e67f95720b1d750
hernandez87v <hernandez87v@hotmail.com>
2020-03-25 20:35:49 -0700

    working on DYnamic Tweets, got something to happen but more work to do with container and pulling tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,59 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function() {
+  // Fake data taken from initial-tweets.json
+  const data = [
+    {
+      user: {
+        name: 'Newton',
+        avatars: 'https://i.imgur.com/73hZDYK.png',
+        handle: '@SirIsaac'
+      },
+      content: {
+        text:
+          'If I have seen further it is by standing on the shoulders of giants'
+      },
+      created_at: 1461116232227
+    },
+    {
+      user: {
+        name: 'Descartes',
+        avatars: 'https://i.imgur.com/nlhLi3I.png',
+        handle: '@rd'
+      },
+      content: {
+        text: 'Je pense , donc je suis'
+      },
+      created_at: 1461113959088
+    }
+  ];
+
+  const createTweetElement = function(tweet) {
+    const $article = $('<article>').addClass('tweets-container');
+    const $header = $('<header>').addClass('head-container');
+    const $userName = $('<div>').text(tweet.user.name);
+    const $userHandle = $('<div>')
+      .addClass('handle')
+      .text('tweet.user.handle');
+    const $content = $('<p>').text(tweet.content.text);
+    const $footer = $('<footer>').addClass('foot-container');
+    const $date = $('<font>').text(tweet.created_at);
+    $header.append($userName);
+    $header.append($userHandle);
+    $article.append($header);
+    $article.append($content);
+    $footer.append($date);
+    console.log(tweet.content.text);
+    console.log(tweet.user.name);
+    return $article;
+  };
+
+  const renderTweets = function(tweets) {
+    for (const tweet of tweets) {
+      $('#tweets-container').append(createTweetElement(tweet));
+    }
+  };
+
+  renderTweets(data);
+});
