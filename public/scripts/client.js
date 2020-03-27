07a23e4ebf1c5f36483c1ac5d572c97398108fee
hernandez87v <hernandez87v@hotmail.com>
2020-03-26 20:28:26 -0700

    tweets are now showing, just need to make a few changes to layout

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,39 +4,7 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-/* <article class="tweets-container">
-  <header class="head-container">
-    <div id="tweet-name-container">
-      <font color="#545149">Vlad</font>
-    </div>
-    <div id="tweet-username-container">
-      <font color="#C8CFE4">@vlad</font>
-    </div>
-  </header>
-  <p class="last-tweet-container">
-    Writing a random tweet for testing and seeing if it works. Also extra long
-    to fix spacing.
-  </p>
-  <footer class="foot-container">
-    <font color="#545149">1 day ago</font>
-  </footer>
-</article>; */
-
 $(document).ready(function() {
-  const loadTweets = () => {
-    $.ajax({
-      method: 'GET',
-      url: '/tweets'
-    })
-
-      .done(function(response) {
-        renderTweets(response);
-      })
-      .fail(function(error) {
-        console.log(`Request failed: ${error.message}`);
-      });
-  };
-
   const createTweetElement = function(tweet) {
     const $article = $('<article>').addClass('tweets-container');
     const $header = $('<header>').addClass('head-container');
@@ -47,39 +15,67 @@ $(document).ready(function() {
     const $content = $('<p>').text(tweet.content.text);
     const $footer = $('<footer>').addClass('foot-container');
     const $date = $('<div>').text(tweet.created_at);
+    // $('<div>').text(moment(tweet.created_at).fromNow());
+    // .text(tweet.created_at);
     $header.append($userName);
     $header.append($userHandle);
     $article.append($header);
     $article.append($content);
     $footer.append($date);
     $article.append($footer);
-    // console.log(tweet.content.text);
-    // console.log(tweet.created_at);
     return $article;
   };
 
-  // $('tweet-block').on('submit', function() {
-  //   if ($('tweet-text').val().length > 140) {
-  //     alert('Your post exceeds 140 characters');
-  //   } else if ($('tweet-text').val().length === '' || null) {
-  //     alert('No Tweet submitted');
-  //   }
-  // });
-
-  const $form = $('form');
-  $form.on('submit', event => {
+  $('.tweet-block').submit(function(event) {
     event.preventDefault();
-    const formData = $form.serialize();
-    $.post('/tweets/', formData).then(res => {
-      // console.log(res);
-    });
+    let $form = $(this).serialize();
+    let $counter = $('.counter');
+    let $error = $('#error-message');
+
+    if ($form === 'text=') {
+      $error.removeClass('hide');
+      $error.text('No tweet to submit!');
+      $counter.text('140');
+    } else if ($form.length > 140) {
+      $error.removeClass('hide');
+      $error.text('Your tweet exceeds 140 characters!');
+    } else {
+      $.ajax({
+        method: 'POST',
+        url: '/tweets',
+        data: $form
+      })
+        .then(() =>
+          $('.tweet-block').each(function() {
+            this.reset();
+            $error.addClass('hide');
+            $counter.text('140');
+          })
+        )
+        .then(data => loadTweets(data));
+    }
   });
 
   const renderTweets = function(tweets) {
+    // tweets.reverse();
     for (const tweet of tweets) {
       $('#tweets-container').append(createTweetElement(tweet));
     }
   };
+
+  const loadTweets = () => {
+    $.ajax({
+      method: 'GET',
+      url: '/tweets'
+    })
+      .done(function(response) {
+        renderTweets(response);
+      })
+      .fail(function(error) {
+        console.log(`Error ${error.message}`);
+      });
+  };
+
   loadTweets();
-  //renderTweets();
+  //renderTweets(tweet);
 });
